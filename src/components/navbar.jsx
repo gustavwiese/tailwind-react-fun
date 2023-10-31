1c20ac9d5b87b96def8c3f1a41a4680dcb0805a3
gustavwiese <gusse12345@gmail.com>
2023-10-31 16:15:44 +0100

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='text-white'> 
-      <h1>REACT</h1>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#3a4c46]">REACT.</h1>
+      <ul className="flex">
+        <li className="p-3">Home</li>
+        <li className="p-3">Company</li>
+        <li className="p-3">Resources</li>
+        <li className="p-3">About</li>
+        <li className="p-3">Contact</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
