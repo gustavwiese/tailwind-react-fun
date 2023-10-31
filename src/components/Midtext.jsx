88926d6df8815f8229a5011e3922d77be2d9e430
gustavwiese <gusse12345@gmail.com>
2023-10-31 19:27:56 +0100

    created some text in the middle

--- src/components/Midtext.jsx
+++ src/components/Midtext.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Hero = () => {
-  return <div className="text-white">hero</div>;
+
+const Midtext = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-1">SOME TEXT GOES HERE</p>
+        <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold md:py-1">H1 text goes here</h1>
+        <div>
+          <p className="md:text-2xl sm:text-xl text-l font-bold">Moving text goes here</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Hero;
+export default Midtext;
