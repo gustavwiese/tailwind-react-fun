3086f890dd22e654c6bb8688146eb3a644a14dea
gustavwiese <gusse12345@gmail.com>
2023-11-01 20:31:27 +0100

    added white component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Midtext from "./components/Midtext";
+import White from "./components/White";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Midtext />
+      <White />
     </div>
   );
 }
---
+++ src/components/White.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function White(){
+  return(
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+
+      </div>
+    </div>
+  )
+}
+
